5848087e2578df7959a7d17593f40c46d9534723
Chupacharcos <adria.morenolozano@gmail.com>
2020-03-12 18:05:41 +0100

    PPorque algo falla

--- Cursos2/src/Cursos/Reservar.java
+++ Cursos2/src/Cursos/Reservar.java
@@ -1,6 +1,10 @@
 package Cursos;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,6 +24,16 @@ public class Reservar extends HttpServlet {
         String payment = request.getParameter("payment");
         
         String password = request.getParameter("password");
+        try {
+			Connection c = SQLiteJDBC.conectar();
+			Statement stmt = null;
+			stmt = c.createStatement();
+			
+
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+
+		}
 	}
 
 }
