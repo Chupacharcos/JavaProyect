72bbd6c4d2f79f745b498d8ccc6ed359bff79ff0
Chupacharcos <adria.morenolozano@gmail.com>
2020-04-10 11:47:09 +0200

    Cambios de rutas

--- Cursos2/src/Cursos/Comentar.java
+++ Cursos2/src/Cursos/Comentar.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/* COMENTARIO DE PRUEBA PARA GIT */
 /**
  * Servlet implementation class Comentar
  */
